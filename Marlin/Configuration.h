9cf231b792ca67047d6e69c04238795e6994d16c
Sergei Ustinov <ustsv@mail.ru>
2025-11-07 14:54:55 +0300

    MCVD4: morpheus_ustsv  FAILED arm-none-eabi-g++ @C:/msys2/tmp/Marlin/build/morpheus_ustsv/tmp2zaln430.tmp Invalid switch - "msys2". *** [C:/msys2/tmp/Marlin/build/morpheus_ustsv/firmware.elf] Error 1
    test            FAILED
    C:/Users/user/.platformio/packages/framework-arduinoststm32-maple/STM32F1/cores/maple/boards.h:39:10: fatal error: board/board.h: No such file or directory

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -94,7 +94,7 @@
  *
  * :[2400, 9600, 19200, 38400, 57600, 115200, 250000, 500000, 1000000]
  */
-#define BAUDRATE 250000
+//#define BAUDRATE 250000
 
 //#define BAUD_RATE_GCODE     // Enable G-code M575 to set the baud rate
 
